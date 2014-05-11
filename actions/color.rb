58f9191233a45044d881986dd97e94f071c8bc53
lukearndt <luke.j.arndt@gmail.com>
2014-05-11 12:17:35 +1000

    Add kelvin to color

--- actions/color.rb
+++ actions/color.rb
@@ -1,11 +1,12 @@
 require 'lifx'
 require_relative '../lifx/lights'
 
-light      = ARGV[0] || 'luke'
-color      = ARGV[1] || 'white'
-brightness = ARGV[2].to_f || 1
+light      =  ARGV[0] || 'luke'
+color      =  ARGV[1] || 'white'
+brightness = (ARGV[2] || 1).to_f
+kelvin     = (ARGV[3] || 3500).to_f
 
-lifx_color = LIFX::Color.send(color, :brightness => brightness)
+lifx_color = LIFX::Color.send(color, :brightness => brightness, :kelvin => kelvin)
 puts "Color: #{lifx_color}"
 
 Lights[light].set_color(lifx_color)
